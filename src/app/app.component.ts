5ce20feca0563fd37f0e11bfedad911bcc83501f
torres10luis <Luis.angel_15@yahoo.com>
2018-06-08 11:45:45 -0700

    new interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+interface Fighter {
+  name: string;
+  points: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+
+  fighter1 = this.fighter1 = new Fighter();
+
+  ngOnInit() {
+  }
 }
