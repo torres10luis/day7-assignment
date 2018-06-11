f688d03b053b7a35ea5ea66f56d35793d39caca1
torres10luis <Luis.angel_15@yahoo.com>
2018-06-11 13:03:03 -0700

    day 7 built in directives done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'I am superman';
+ 
+
+  users = [{
+    name: 'test user',
+    age: 30
+   }, {
+    name: ' test user 2',
+    age: 2
+  }];
+
+
+
+  displayParagraph = true;
+  // displayParagraph = false; here is the other one that i use to make sure this work.
 }
